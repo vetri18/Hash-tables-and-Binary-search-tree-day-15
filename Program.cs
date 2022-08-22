c4b8fbdb56069469f1f8f307f999eae11060581e
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-22 19:53:31 +0530

    search 63  in the Binary Tree uc 6

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace EmployeeWage
+﻿namespace BinaryTree
 {
-    class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Hash Tables and Binary Search Tree program");
+            Console.WriteLine("Wellcome to Hash Table and Binary Search Tree");
+
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            binarySearch.Insert(22);
+            binarySearch.Insert(40);
+            binarySearch.Insert(60);
+            binarySearch.Insert(95);
+            binarySearch.Insert(11);
+            binarySearch.Insert(65);
+            binarySearch.Insert(3);
+            binarySearch.Insert(16);
+            binarySearch.Insert(63);
+            binarySearch.Insert(67);
+
+            binarySearch.Display();
+
+            binarySearch.GetSize();
+
+            Console.WriteLine(" ");
+
+            bool result = binarySearch.IfExists(63, binarySearch);
+            Console.WriteLine(result);
         }
     }
 }
